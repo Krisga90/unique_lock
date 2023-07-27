2c9c08cfde3da5c41d130e2e548f81507efcfb46
Krisga <krzysiekgdynia@poczta.fm>
2023-07-27 11:34:42 +0200

    clean up

--- main.cpp
+++ main.cpp
@@ -8,17 +8,20 @@ using namespace std::literals;
 
 std::mutex PRINT_MUTEX;
 
-void printWithMutex(const std::string& str, bool throw_exc);
-void printWithLockGuard(const std::string& str, bool throw_exc);
-void printWithUniqueLock(const std::string& str, bool throw_exc);
-void printWithUniqueLockV2(const std::string& str, bool throw_exc);
+void printWithMutex(const std::string &str, bool throw_exc);
+void printWithLockGuard(const std::string &str, bool throw_exc);
+void printWithUniqueLock(const std::string &str, bool throw_exc);
+void printWithUniqueLockV2(const std::string &str, bool throw_exc);
 
 int main() {
   std::string text = "Fizzz Buzzz!!";
   bool throw_except_argument = false;
-  std::thread thr1(printWithUniqueLockV2, std::cref(text), throw_except_argument);
-  std::thread thr2(printWithUniqueLockV2, std::cref(text), throw_except_argument);
-  std::thread thr3(printWithUniqueLockV2, std::cref(text), throw_except_argument);
+  std::thread thr1(printWithUniqueLockV2, std::cref(text),
+                   throw_except_argument);
+  std::thread thr2(printWithUniqueLockV2, std::cref(text),
+                   throw_except_argument);
+  std::thread thr3(printWithUniqueLockV2, std::cref(text),
+                   throw_except_argument);
 
   thr1.join();
   thr2.join();
@@ -27,107 +30,81 @@ int main() {
   return 0;
 }
 
-void printWithMutex(const std::string& str,bool throw_exc)
-{
-  try
-  {
+void printWithMutex(const std::string &str, bool throw_exc) {
+  try {
     PRINT_MUTEX.lock();
 
-    for(char c : str)
-    {
+    for (char c : str) {
       std::cout << c;
     }
     std::cout << '\n';
 
-    if(throw_exc)
-    {
+    if (throw_exc) {
       // throw std::exception();
       throw std::runtime_error("Forced error");
     }
     PRINT_MUTEX.unlock();
     std::this_thread::sleep_for(500ms);
-  }
-  catch(const std::exception& e)
-  {
+  } catch (const std::exception &e) {
     std::cerr << e.what() << '\n';
   }
-  
 }
-void printWithLockGuard(const std::string& str, bool throw_exc)
-{
-  try
-  {
-    std::lock_guard lg(PRINT_MUTEX);
+void printWithLockGuard(const std::string &str, bool throw_exc) {
+  try {
+    std::lock_guard<std::mutex> lg(PRINT_MUTEX);
 
-    for(char c : str)
-    {
+    for (char c : str) {
       std::cout << c;
     }
     std::cout << '\n';
 
-    if(throw_exc)
-    {
+    if (throw_exc) {
       // throw std::exception();
       throw std::runtime_error("Forced error");
     }
     std::this_thread::sleep_for(500ms);
-  }
-  catch(const std::exception& e)
-  {
+  } catch (const std::exception &e) {
     std::cerr << e.what() << '\n';
   }
-  
 }
-void printWithUniqueLock(const std::string& str, bool throw_exc)
-{
-  try
-  {
-    std::unique_lock u_lock(PRINT_MUTEX);
+void printWithUniqueLock(const std::string &str, bool throw_exc) {
+  try {
+    std::unique_lock<std::mutex> u_lock(PRINT_MUTEX);
 
-    for(char c : str)
-    {
+    for (char c : str) {
       std::cout << c;
     }
     std::cout << '\n';
 
-    if(throw_exc)
-    {
+    if (throw_exc) {
       // throw std::exception();
       throw std::runtime_error("Forced error");
     }
     u_lock.unlock();
     std::this_thread::sleep_for(500ms);
-  }
-  catch(const std::exception& e)
-  {
+  } catch (const std::exception &e) {
     std::cerr << e.what() << '\n';
   }
 }
 
-void printWithUniqueLockV2(const std::string& str, bool throw_exc)
-{
-  try
-  {
-    std::unique_lock u_lock(PRINT_MUTEX, std::defer_lock);
+void printWithUniqueLockV2(const std::string &str, bool throw_exc) {
+  try {
+    std::unique_lock<std::mutex> u_lock(PRINT_MUTEX, std::defer_lock);
     u_lock.lock();
-    std::unique_lock u_lock2(PRINT_MUTEX, std::adopt_lock);
-    for(char c : str)
-    {
+    std::unique_lock<std::mutex> u_lock2(PRINT_MUTEX, std::adopt_lock);
+    for (char c : str) {
       std::cout << c;
     }
     std::cout << '\n';
 
-    if(throw_exc)
-    {
+    if (throw_exc) {
       // throw std::exception();
       throw std::runtime_error("Forced error");
     }
     // u_lock.unlock();
     u_lock2.unlock();
     std::this_thread::sleep_for(500ms);
-  }
-  catch(const std::exception& e)
-  {
+  } catch (const std::exception &e) {
     std::cerr << e.what() << '\n';
   }
 }
